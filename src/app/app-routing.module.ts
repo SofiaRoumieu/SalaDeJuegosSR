cfdd384a64f22df664542259d62bfe6a3816d42e
SofiaRoumieu <roumieusofia@gmail.com>
2021-10-02 14:40:02 -0300

    nuevos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { QuienSoyComponent } from './Componentes/quien-soy/quien-soy.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { RegistroComponent } from './Componentes/registro/registro.component';
+import { HomeComponent } from './Componentes/home/home.component';
+
+
+const routes: Routes=[
+  {path:'QuienSoy', component: QuienSoyComponent},
+  {path:'Login', component: LoginComponent},
+  {path:'Registro', component:RegistroComponent},
+  {path:'Home', component: HomeComponent},
+  {path:'', redirectTo:'Home', pathMatch:'full'},
+  //{path:'**', component: PageNotFoundComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports:[RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Componentes/home/home.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { QuienSoyComponent } from './Componentes/quien-soy/quien-soy.component';
+import { RegistroComponent } from './Componentes/registro/registro.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    QuienSoyComponent,
+    RegistroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
